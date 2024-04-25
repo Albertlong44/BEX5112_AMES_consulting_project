0f231d1efd7e52e6aef8ccda34841be16d1265f7
Albertlong44 <ylon0012@student.monash.edu>
2024-04-25 19:47:04 +1000

    Fixed the fixed header in dataTable

--- AMESdashboard/app.R
+++ AMESdashboard/app.R
@@ -319,11 +319,10 @@ output$DTmodel<- renderDT({
                                               `Short description` =Short.description,
                                               `Consol CBM` =consolidated_cbm,
                                               `Net profit` =Profit_earned,
-                                               Brand =Brand_name,
                                               `Allocation reult`=allocation_result_final,
                                               `Warning message`=Warning_message)%>%
     mutate(Month =as.character(Month)) %>%
-    select(`Product code`,Brand, Month,Region,`Consol CBM`,`Net profit` ,`Allocation reult`,`Warning message`) %>% 
+    select(`Product code`,`Short description`, Month,Region,`Consol CBM`,`Net profit` ,`Allocation reult`,`Warning message`) %>% 
     datatable(
       callback=JS('
     $("button.buttons-copy").css({
@@ -345,11 +344,13 @@ output$DTmodel<- renderDT({
 ')
       
       ,
-      extensions = c('Buttons', 'Scroller'),
+      extensions = c('Buttons', 'Scroller','FixedColumns'),
       options = list(
         scrollY = 500,
         scroller = TRUE,
         dom = 'Bfrtip',
+        scrollX = TRUE,
+        fixedColumns = TRUE,
         
         buttons = list( list(extend = "copy", text = '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"/></svg>'), 
                         list(extend = "csv", text = '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>')) ,
